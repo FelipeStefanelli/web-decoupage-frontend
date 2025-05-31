84fedafec779ce560f343b352dcc52de70475c95
Felipe S A Silva <felipe.stefanelli.tech@gmail.com>
2025-05-31 15:26:40 -0300

    First commit

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  base: isProduction ? './' : '/',
   plugins: [react()],
-  build: {
-    outDir: '../frontend/dist',
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
+  }
 });
